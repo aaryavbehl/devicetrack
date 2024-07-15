7c110684d24be46ef523c12897de40bf6179852e
Aaryav Behl <aaryavbehl47@gmail.com>
2024-07-15 12:50:42 +0530

    modified:   package.json

--- app.js
+++ app.js
@@ -10,14 +10,16 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on("connection", function (socket){
-    socket.on("send-location", function(data){
-        io.emit
+    socket.on("send-location", function (data){
+        io.emit("recieve-location", {id: socket.id, ...data });
     });
-    console.log("Connected");
+    socket.on("disconnect", function(){
+        io.emit("user-disconnected", socket.id)
+    })
 });
 
 app.get("/", function (req, res){
     res.render("index");
 });
 
-server.listen(3000);
+server.listen(3100);
